89c7f39aa31c4c1f6ef19a923cac222af3d367c7
tcv <tcv@tcv.com>
2020-08-27 14:03:07 +0700

    Add new component 'Profile'

---
+++ UpStragram/src/components/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1> {{ msg }} </h1>
+</template>
+
+<script>
+export default {
+    name: 'Profile',
+    data: function () {
+        return {
+            msg: "This is my profile component"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
